7f55881795b184510836de16f467debf09d397fc
nakaji <nakaji126@gmail.com>
2011-03-23 23:29:10 +0900

    通常のツイート判定を実装

---
+++ MotsunabeZombieProject.Test/TweetCategorizerTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace MotsunabeZombieProject.Test
+{
+    [TestFixture]
+    public class TweetCategorizerTest
+    {
+        [Test]
+        public void 通常のツイート()
+        {
+            var ct = new TweetCategorizer();
+            Assert.That(ct.Categorize("nakaji\tほげ"), Is.EqualTo("Normal"));
+        }
+    }
+}
---
+++ MotsunabeZombieProject/TweetCategorizer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MotsunabeZombieProject
+{
+    public class TweetCategorizer
+    {
+        public object Categorize(string p)
+        {
+            return "Normal";
+        }
+    }
+}
